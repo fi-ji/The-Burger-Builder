27f38df4010a5ebc2bec2e8aa034c4660dbe8b1e
Fidan Hajiyeva <57771443+fi-ji@users.noreply.github.com>
2020-12-29 00:42:21 +0400

    Added More/Less build controls

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,5 +1,28 @@
 import React from 'react'
+import classes from './BuildControls.css'
+import BuildControl from '../BuildControl/BuildControl'
 
-const buildControls = (props) => {}
+const controls = [
+  { label: 'Salad', type: 'salad' },
+  { label: 'Bacon', type: 'bacon' },
+  { label: 'Cheese', type: 'cheese' },
+  { label: 'Meat', type: 'meat' },
+]
+
+const buildControls = (props) => {
+  return (
+    <div className={classes.BuildControls}>
+      {controls.map((ctrl) => (
+        <BuildControl
+          key={ctrl.label}
+          label={ctrl.label}
+          added={() => props.ingredientAdded(ctrl.type)}
+          removed={() => props.ingredientRemoved(ctrl.type)}
+          disabled={props.disabled[ctrl.type]}
+        />
+      ))}
+    </div>
+  )
+}
 
 export default buildControls
